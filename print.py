e6b68eb8ce1c529eddcaff4ae39bb5bf14fd022a
ShriyaMatt <happydays10413@gmail.com>
2025-10-09 19:28:24 +0100

    Add files via upload

---
+++ print.py
@@ -0,0 +1,6 @@
+print("Welcome to the World of Coding!")
+#Printing a Number
+print(45)
+print("This is my firts program \n I am excited to learn Python")
+print("Hi my age is", 12 )
+print("Welcome to", end="&&&&&")
