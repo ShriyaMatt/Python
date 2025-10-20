284ffaa7ab21ac57d726835a1d26fa46fb4d3cde
ShriyaMatt <happydays10413@gmail.com>
2025-10-20 18:46:13 +0100

    Add files via upload

---
+++ typecasting.py
@@ -0,0 +1,17 @@
+name= "Penguin"
+age= 15
+is_student= True
+weight= 38.5
+
+#Printing Differnt Variables and their Data Type
+print("Name:", name)
+print("Print Data Type of Name is:", type(name))
+
+print("Age:", age)
+print("Print Data Type of Age is:", (age))
+
+print("is_student:", is_student)
+print("Print Data Type of is_student is:", (is_student))
+
+print("Weight:", weight)
+print("Print Data Type of Weight is:", (weight))
---
+++ user input.py
@@ -0,0 +1,9 @@
+text=str(input("Enter a String: "))
+
+# Reverse String
+# using step value as -1 to irate in reverse
+revText=text[::-1]
+text=revText
+
+print("Reverse of Given Srting is:")
+print(text)
