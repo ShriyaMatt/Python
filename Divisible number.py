e537b24faca4ad1bd67bf3801c67969208ccbd8e
ShriyaMatt <happydays10413@gmail.com>
2025-11-13 18:27:13 +0000

    Add files via upload

---
+++ Divisible number.py
@@ -0,0 +1,8 @@
+num=int(input("Enter the Number :"))
+divosor=int(input("Enter the Divosor: "))
+
+#Check Divisiblty
+if num%divosor==0:
+    print(f"{num} is divisble by {divosor}")
+else:
+    print(f"{num} is not divisble by {divosor}")
