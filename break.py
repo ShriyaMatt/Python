ab23d9bca738d92cae5a2266e820cc849c0bd041
ShriyaMatt <happydays10413@gmail.com>
2025-12-24 16:08:44 +0000

    Add files via upload

---
+++ break.py
@@ -0,0 +1,10 @@
+a=input("Enter a word:")
+#program to check break keyword
+for i in a: #iterate for loop
+  if(i=='A'): #condidtion 1
+  #display result
+    print("A is found")
+    break #break statment
+  else:
+  #display result
+    print("A not found")
---
+++ continue.py
@@ -0,0 +1,8 @@
+var=10               #inistilase
+while var>0:         #iterate loop
+    var=var-1
+    if var==5:
+       continue
+       #display result
+    print('\nCurrent Varaible Value:', var)
+print("\nGood Bye!")
