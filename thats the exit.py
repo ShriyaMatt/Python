153bbcc030969079d2bd89a1b5f4bbbbc393c720
ShriyaMatt <happydays10413@gmail.com>
2026-01-22 18:40:42 +0000

    Add files via upload

---
+++ thats the exit.py
@@ -0,0 +1,12 @@
+#python program to demonstare
+#exit()
+for i in range(10):
+  #if the value of r becomes
+  #5 then the program is forced
+  #to exit
+  if i==5:
+
+    #prints the exit message
+    print(exit)
+    exit()
+  print(i)
